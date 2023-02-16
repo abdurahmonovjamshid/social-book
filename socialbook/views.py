0cfac7a82547dde91c6ae242f4aa67c5ce054203
unknown <jamshidabdurahmonov060@gmail.com>
2023-02-16 17:45:56 +0500

    upload post view

--- socialbook/views.py
+++ socialbook/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Post
 from django.http import HttpResponse
 
 
@@ -10,7 +10,22 @@ from django.http import HttpResponse
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_obj = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_obj)
+    return render(request, 'index.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
 
 
 @login_required(login_url='signin')
