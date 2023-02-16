75ef5335dd4a1f24e5241c48c792a91aa46af980
unknown <jamshidabdurahmonov060@gmail.com>
2023-02-16 15:55:43 +0500

    configuring user profile setting

--- socialbook/views.py
+++ socialbook/views.py
@@ -13,6 +13,25 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        image = user_profile.profileimg
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+        bio = request.POST['bio']
+        location = request.POST['location']
+
+        user_profile.profileimg = image
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
 def signup(request):
     try:
         if request.method == 'POST':
@@ -31,10 +50,13 @@ def signup(request):
                     user = User.objects.create_user(username=username, email=email, password=password)
                     user.save()
 
+                    user_login = auth.authenticate(username=username, password=password)
+                    auth.login(request, user_login)
+
                     user_model = User.objects.get(username=user)
                     new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                     new_profile.save()
-                    redirect('signup')
+                    redirect('settings')
             else:
                 messages.info(request, 'Password Not Matching')
                 return redirect('signup')
@@ -58,6 +80,7 @@ def signin(request):
     return render(request, 'signin.html')
 
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
