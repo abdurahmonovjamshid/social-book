7bcb2a7093dda9f4f0008250c7941ed869c0896d
unknown <jamshidabdurahmonov060@gmail.com>
2023-02-16 18:46:21 +0500

    like and unlike posts

--- socialbook/urls.py
+++ socialbook/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload', views.upload, name='upload'),
     path('settings', views.settings, name='settings'),
+    path('like-post', views.like_post, name='like-post'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
