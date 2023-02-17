148288a1cfcc71a7f55278da729dd7f4c3dabc85
unknown <jamshidabdurahmonov060@gmail.com>
2023-02-17 11:27:05 +0500

    deploy

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wdr+5ed_!*w4+)imf$llbqsmlp$$(^d*!9f80y$@uv&oe=vyq6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
